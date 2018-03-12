f7c34ed5d92308b214a2b5ce077db791a9efa6b4
Frank Edwards <azhreifje@gmail.com>
2018-03-11 21:04:34 -0400

    More key testing

--- .travis/builder.sh
+++ .travis/builder.sh
@@ -1,22 +1,21 @@
 #!/usr/bin/env bash
 
 # Either runs a normal './gradlew build' or, if this CI run is because
-# of TRAVIS_TAG, runs './gradlew bintrayUpload' instead.
+# of a tagged commit on the RPTools repository, runs
+# './gradlew bintrayUpload' instead.
 
 # For JDK9, should add:
 #   _JAVA_OPTIONS="${JAVA_OPTIONS} --illegal-access=permit"
-# .. in order to get rid of the error messages sent to the console.
+# .. in order to get rid of the warning messages sent to the console.
 #
 # Not needed at the current time, though, since the RPTools
 # libraries can be built using JDK8.
 
-set -ev
+set -vex
 
 if [ "X$TRAVIS_TAG" = "X" ] || [ "X$TRAVIS_REPO_SLUG" != "RPTools/rplib" ]; then
-    echo "Building; no bintray upload."
     ./gradlew --no-daemon build
 else
-    echo "Building; will upload to bintray when finished."
     ./gradlew --no-daemon bintrayUpload \
         "-Dtag=$TRAVIS_TAG" \
         "-Dpublish=true" \
